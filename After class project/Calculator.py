792f0f7e8035a82a78eac605489599df578c9943
Aarush2710 <aarush.indulkar2710@gmail.com>
2026-03-15 19:18:43 +0530

    Add files via upload

---
+++ After class project/Calculator.py
@@ -0,0 +1,4 @@
+a=int(input("Enter the num1 " ))
+b=int(input("Enter the num2 " ))
+result=a+b
+print(result)
---
+++ After class project/square root.py
@@ -0,0 +1,3 @@
+import math
+num=int(input("Enter the number to find square root  "))
+print(math.sqrt(num))
