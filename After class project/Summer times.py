bab6876c0ed71e90a48cb97e63184a55742ad0a7
Aarush2710 <aarush.indulkar2710@gmail.com>
2026-03-21 10:59:27 +0530

    Add files via upload

---
+++ After class project/Summer times.py
@@ -0,0 +1,8 @@
+age=int(input("Enter your age  "))
+if age > 18 :
+    print("You can vote")
+elif age == 18 :
+    print("You can vote")
+else :
+    print("You cannot vote")
+    print("Your age should be 18 or above to vote")
